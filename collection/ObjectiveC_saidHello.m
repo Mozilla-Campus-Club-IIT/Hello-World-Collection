97276855ab68fca6b4f843e2864bfb8c199fe64a
Öbhasha <obhasha@live.com>
2017-10-25 01:45:52 +0530

    Hello World by Objective C #98 #hacktoberfest

---
+++ collection/ObjectiveC_saidHello.m
@@ -0,0 +1,11 @@
+// Hello World by Objective C
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"Hello, World");
+        [pool drain];
+        return 0;
+}
