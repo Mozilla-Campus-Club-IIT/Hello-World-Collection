79929b71928ca0eb4f571d604f579a356dcfd757
zachRudz <zachRudz@users.noreply.github.com>
2017-10-23 21:24:46 -0400

    Create bash_saidHello.sh
    Creation of initial bash_saidHello.sh file.

---
+++ collection/bash_saidHello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ^^ The location of the bash interpreter.
+
+# Make sure to chmod +x, so that you can execute this script!
+#chmod +x ./bash_saidHello.sh
+
+# Printing to the terminal
+greeting="hello world"
+echo "$greeting"
