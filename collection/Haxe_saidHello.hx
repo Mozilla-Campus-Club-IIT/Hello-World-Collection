0c8feb93e2f45fd5fc822c05ea9b346ee51aeb1e
Kevin Weiss <omar@sloppylinux.com>
2018-10-17 21:08:55 -0500

    Fixed comment format to actually work

--- collection/Haxe_saidHello.hx
+++ collection/Haxe_saidHello.hx
@@ -1,14 +1,14 @@
-# Haxe is an open source toolkit based on a modern, high level, strictly typed 
-# programming languange, a cross-compiler, a complete cross-platform standard 
-# library and ways to access each platform's native capabilities.
+/**
+ Haxe is an open source toolkit based on a modern, high level, strictly typed 
+ programming languange, a cross-compiler, a complete cross-platform standard 
+ library and ways to access each platform's native capabilities.
+ 
+ This hello world application can be run by executing the following commands:
+   HAXE_STD_PATH=/path/to/haxe/std /path/to/haxe -main Haxe_saidHello --interp
 
-# 
-# This hello world application can be run by executing the following commands:
-
-# HAXE_STD_PATH=/path/to/haxe/std /path/to/haxe -main Haxe_saidHello --interp
-
-# Output:
-#   Haxe_saidHello.hx:3: Hello World
+ Output:
+   Haxe_saidHello.hx:3: Hello World
+**/
 
 class Haxe_saidHello {
   static public function main():Void {
